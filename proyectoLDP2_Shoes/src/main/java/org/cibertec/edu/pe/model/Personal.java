a597761d11093bee1663512c2bf4d4ec89e452e6
DiegoPeregrino <159731582+DiegoPeregrino@users.noreply.github.com>
2024-02-25 23:52:13 -0500

    Segunda version
    Se agrego métodos para la clase Personal, Producto, Proveedor, VentasDet.
    queda pendiente métodos para clase VentasCab.

--- proyectoLDP2_Shoes/src/main/java/org/cibertec/edu/pe/model/Personal.java
+++ proyectoLDP2_Shoes/src/main/java/org/cibertec/edu/pe/model/Personal.java
@@ -1,5 +1,68 @@
 package org.cibertec.edu.pe.model;
 
-public class Personal {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+	
+	
+	@Entity
+	@Table(name = "Personal")
+	public class Personal {
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private int idPersonal; 
+	    private String nombreper; 
+	    private String telefonoper; 
+	    private String direccionper;
+	    
+	    
+	    //Constructores
+	    
+		public Personal() {
+		}
 
-}
+		public Personal(int idPersonal, String nombreper, String telefonoper, String direccionper) {
+		
+			this.idPersonal = idPersonal;
+			this.nombreper = nombreper;
+			this.telefonoper = telefonoper;
+			this.direccionper = direccionper;
+		}
+		//Getters and Setters
+
+		public int getIdPersonal() {
+			return idPersonal;
+		}
+
+		public void setIdPersonal(int idPersonal) {
+			this.idPersonal = idPersonal;
+		}
+
+		public String getNombreper() {
+			return nombreper;
+		}
+
+		public void setNombreper(String nombreper) {
+			this.nombreper = nombreper;
+		}
+
+		public String getTelefonoper() {
+			return telefonoper;
+		}
+
+		public void setTelefonoper(String telefonoper) {
+			this.telefonoper = telefonoper;
+		}
+
+		public String getDireccionper() {
+			return direccionper;
+		}
+
+		public void setDireccionper(String direccionper) {
+			this.direccionper = direccionper;
+		}	    
+	}
+
--- proyectoLDP2_Shoes/src/main/java/org/cibertec/edu/pe/model/VentasCab.java
+++ proyectoLDP2_Shoes/src/main/java/org/cibertec/edu/pe/model/VentasCab.java
@@ -1,5 +1,6 @@
 package org.cibertec.edu.pe.model;
 
-public class VentasCab {
+	public class VentasCab {
+		}			
+	
 
-}
